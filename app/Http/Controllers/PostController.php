a15540085336da5f85da8f1b8edc3291267eb60e
Kevin Feitosa <kevin.feitosa11@outlook.com>
2024-02-22 01:11:49 -0300

    intial commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index() {
+        $posts = Post::orderBy('created_at', 'desc')->get();
+
+        $postsOrganized = [];
+        foreach ($posts as $post) {
+            $year = $post->created_at->format('Y');
+            $month = $post->created_at->format('F');
+            $postsOrganized[$year][$month][] = $post;
+        }
+
+        return view('welcome', compact('postsOrganized'));
+    }
+
+
+    public function create() {
+        return view('posts.create');
+    }
+
+    public function show($id) {
+        $post = Post::findOrFail($id);
+        return view('posts.show', ['post' => $post]);
+    }
+
+    public function store(Request $request) {
+        $post = new Post;
+
+        $post->title = $request->title;
+        $post->body = $request->body;
+        
+        $tags = $request->tags;
+        
+        $tags = array_filter($tags);
+        $tags = array_values($tags);
+        $tags = explode(' ', $tags[0]);
+        $post->tags = $tags;
+        
+        $post->save();
+        
+        return redirect('/');
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.main')
+
+@section('title', 'MakitaOnRails')
+
+@section('content')
+<div class="max-w-screen-md mx-auto p-4">
+    <div class="flex flex-col gap-5">
+        @foreach($postsOrganized as $year => $postsByYear)
+            @foreach($postsByYear as $month => $postsByMonth)
+                <div class="flex items-center justify-center bg-akita w-min whitespace-nowrap h-14 px-5">
+                    <a href="#{{ date('Y-m', strtotime($year . '-' . $month)) }}">
+                        <h3 class="text-xl font-bold text-white">{{ $year }} - {{ date('F', strtotime($month)) }}</h3>
+                    </a>
+                </div>
+                @foreach($postsByMonth as $post)
+                    <div id="{{ date('Y-m', strtotime($year . '-' . $month)) }}" class="bg-card w-full min-h-max px-4 py-2">
+                        <a href="/posts/{{ $post->id }}">
+                            <h4 class="text-akita font-extrabold text-2xl">{{ $post->title }}</h4>
+                            <div class="w-1/4 bg-divider mt-2 mb-1" style="height: 1px;"></div>
+                            <h3 class="text-akita font-medium text-sm">
+                                {{ $post->created_at->format('Y F j, H:i') }} h |
+                                <span>
+                                    @foreach($post->tags as $tag)
+                                    <span>
+                                        <a href="/{{ $tag }}" class="font-bold hover:underline">{{ $tag }}</a>
+                                    </span>
+                                    @endforeach
+                                </span>
+                            </h3>
+                        </a>
+                    </div>
+                @endforeach
+            @endforeach
+        @endforeach
+    </div>
+</div>
+@endsection
