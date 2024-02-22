2a6129db6edf3b109a098c23161d6365723af4fd
Kevin Feitosa <kevin.feitosa11@outlook.com>
2024-02-22 14:08:44 -0300

    feat: add writer middleware

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/',[PostController::class, 'index']);
-Route::get('/posts/create', [PostController::class, 'create']);
+Route::get('/posts/create', [PostController::class, 'create'])->middleware('auth')->middleware('writer');
 Route::get('/posts/{id}', [PostController::class, 'show']);
 Route::post('/posts', [PostController::class, 'store']);
 
